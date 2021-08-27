85140c8408c979599a9b734116f703f6647e23e7
rayyanabubakr <71322861+rayyanabubakr@users.noreply.github.com>
2021-08-27 12:09:45 +0530

    Update Get Maze Path With Jumps.cpp

--- Recursion with Arraylists/Get Maze Path With Jumps.cpp
+++ Recursion with Arraylists/Get Maze Path With Jumps.cpp
@@ -4,76 +4,62 @@
 using namespace std;
 vector<string> getMaze(int sr, int sc, int dr, int dc)
 {
-    if(sr==dr && sc==dc)
+//   if (sr > dr || sc > dc) {
+//     vector<string> base;
+//     return base;
+//   }
+  if (sr == dr && sc == dc)
+  {
+    vector<string> base;
+    base.push_back("");
+    return base;
+  }
+  vector<string> ans;
+  for (int i = 1; i <= dc - sc; i++)
+  {
+    vector<string> H = getMaze(sr, sc + i, dr, dc);
+    for (string s : H)
     {
-        vector<string> base;
-        base.push_back("");
-        return base;
+      ans.push_back("h" + to_string(i) + s);
     }
-    vector<string> ans;
-    if(sc+1<=dc)
-    {
-        int c=1,d=1;
-        vector<string> H= getMaze(sr,sc+1,dr,dc);
-        for(string s: H)
-        {
-            cout<<s<<"#"<<"\n";
-            if(s=="h")
-            {
-                c++;
-            }
-            if(s=="v")
-            {
-                d++;
-            }
-        }
-        ans.push_back("h"+to_string(c)+"v"+to_string(d));
-    }
-    if(sr+1<=dr)
-    {
-        int c=1,d=1;
-        vector<string> V= getMaze(sr+1,sc,dr,dc);
-        for(string s: V)
-        {
-            if(s=="v")
-            {
-                c++;
-            }
-            if(s=="h")
-            {
-                d++;
-            }
-        }
-        ans.push_back("v"+to_string(c)+"h"+to_string(d));
 
-    }
-    if(sc+1<=dc && sr+1<=dr)
+  }
+  for (int i = 1; i <= dr - sr; i++)
+  {
+    vector<string> V = getMaze(sr + i, sc, dr, dc);
+    for (string s : V)
     {
-        int c=1,d=1;
-        vector<string> D= getMaze(sr+1,sc+1,dr,dc);
-        for(string s: D)
-        {
-            c++;
-        }
-        ans.push_back("d"+to_string(c));
+      ans.push_back("v" + to_string(i) + s);
     }
-    return ans;
+
+  }
+  for (int i = 1; i <= dc - sc && i <= dr - sr; i++)
+  {
+    vector<string> D = getMaze(sr + i, sc + i, dr, dc);
+    for (string s : D)
+    {
+      ans.push_back("d" + to_string(i) + s);
+    }
+
+  }
+  return ans;
 }
-void display(vector<string>& arr){
-    cout << "[";
-    for(int i = 0;i < arr.size();i++){
-        cout << arr[i];
-        if(i < arr.size() -1) cout << ", ";
-    }
-    
-    cout << "]"<<endl;
+void display(vector<string>& arr) {
+  cout << "[";
+  for (int i = 0; i < arr.size(); i++) {
+    cout << arr[i];
+    if (i < arr.size() - 1) cout << ", ";
+  }
+
+  cout << "]" << endl;
 }
 
 
 int main() {
-    int n,m; cin >> n >> m;
-    vector<string> ans = getMaze(0,0,n-1,m-1);
-    display(ans);
+  int n, m;
+  cin >> n >> m;
+  vector<string> ans = getMaze(0, 0, n - 1, m - 1);
+  display(ans);
 
-    return 0;
+  return 0;
 }
